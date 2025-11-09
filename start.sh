ae6e8355afc98c09591f7a723be31f2011244100
Ruhan116 <capablecoder01@gmail.com>
2025-11-09 21:03:00 +0600

    Siuuu

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-python manage.py migrate
+python manage.py migrate --noinput
 
-PORT=${PORT:-8000}
-gunicorn project.wsgi --bind 0.0.0.0:$PORT --timeout 120 --log-file -
+PORT=${PORT:-8080}
+echo "[start.sh] Starting Gunicorn on port $PORT"
+exec gunicorn project.wsgi --bind 0.0.0.0:$PORT --timeout 120 --log-file -
